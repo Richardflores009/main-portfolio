dae378bb7827e7f1f6fc3640d91501b466c2230b
Dick Flowers <dickflowers@DickMiniPro.local>
2021-01-14 15:44:22 -0700

    new features for gatsby

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/dickflowers/Desktop/deploy/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/dickflowers/Desktop/deploy/node_modules/gatsby-plugin-offline/app-shell.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/dickflowers/Desktop/deploy/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/dickflowers/Desktop/deploy/src/pages/index.js")))
 }
